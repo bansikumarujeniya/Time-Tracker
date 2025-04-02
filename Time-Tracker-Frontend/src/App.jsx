47a85f960caccabf3527d08f924e5f04a1038f80
Bansikumar Ujeniya <155151038+bansikumarujeniya@users.noreply.github.com>
2025-04-02 16:40:40 +0530

    Update App.jsx

--- Time-Tracker-Frontend/src/App.jsx
+++ Time-Tracker-Frontend/src/App.jsx
@@ -1,15 +1,13 @@
 import { useState, useEffect } from "react";
 import { Routes, Route, Navigate, useLocation } from "react-router-dom";
+import axios from "axios";
 import "./assets/css/adminlte.css";
 import "./assets/css/adminlte.min.css";
+import "./App.css";
+
 import { UserSidebar } from "./components/layouts/UserSidebar";
 import { Login } from "./components/common/Login";
 import { Signup } from "./components/common/Signup";
-import "./App.css";
-import { AdminSidebar } from "./components/admin/AdminSidebar";
-import { DeveloperSidebar } from "./components/developer/DeveloperSidebar";
-import { ManagerSidebar } from "./components/manager/ManagerSidebar";
-import axios from "axios";
 import { RegisterSelection } from "./components/common/RegisterSelection";
 import { AddProjectForm } from "./components/manager/AddProjectForm";
 import PrivateRoutes from "./hooks/PrivateRoutes";
@@ -27,6 +25,9 @@ import DeveloperTimeLogger from "./components/developer/DeveloperTimeLogger";
 import { ResetPassword } from "./components/common/ResetPassword";
 import { ForgotPassword } from "./components/common/ForgotPassword";
 import DeveloperTimeLogList from "./components/developer/DeveloperTimeLogList";
+import { AdminSidebar } from "./components/admin/AdminSidebar";
+import { DeveloperSidebar } from "./components/developer/DeveloperSidebar";
+import { ManagerSidebar } from "./components/manager/ManagerSidebar";
 import AdminDashboard from "./components/admin/AdminDashboard";
 import AdminUserManagement from "./components/admin/AdminUserManagement";
 import AdminModuleManagement from "./components/admin/AdminModuleManagement";
@@ -36,69 +37,82 @@ import AdminTimeLogManagement from "./components/admin/AdminTimeLogManagement";
 import AdminBilling from "./components/admin/AdminBilling";
 import AdminReport from "./components/admin/AdminReport";
 
+const API_URL = "https://time-tracker-68a2.onrender.com";
+axios.defaults.baseURL = API_URL;
+
+const loginUser = async (email, password) => {
+    try {
+        const response = await axios.post(
+            `/users/login`,
+            { email, password },
+            { withCredentials: true } // Ensure cookies & headers are sent
+        );
+        return response.data;
+    } catch (error) {
+        console.error("Login Error:", error);
+        throw error;
+    }
+};
 
 function App() {
+    const location = useLocation();
 
-  axios.defaults.baseURL = "https://time-tracker-68a2.onrender.com"
+    useEffect(() => {
+        if (location.pathname === "/login" || location.pathname === "/signup") {
+            document.body.className = "";
+        } else {
+            document.body.className =
+                "layout-fixed sidebar-expand-lg bg-body-tertiary sidebar-open app-loaded";
+        }
+    }, [location.pathname]);
 
-  const location = useLocation();
-
-  useEffect(() => {
-    if (location.pathname === "/login" || location.pathname === "/signup") {
-      document.body.className = "";
-    } else {
-      document.body.className =
-        "layout-fixed sidebar-expand-lg bg-body-tertiary sidebar-open app-loaded";
-    }
-  }, [location.pathname]);
-
-  return (
-    <div className={location.pathname === "/login" || location.pathname === "/signup" ? "" : "app-wrapper"}>
-      <Routes>
-        <Route path="/" element={<Navigate to="/landingpage" replace />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/register" element={<RegisterSelection />} />
-        <Route path="/landingpage" element={<LandingPage />} />
-        <Route path="/resetpassword" element={<ResetPassword/>} />
-        <Route path="/resetpassword/:token" element={<ResetPassword />} />
-        <Route path="/forgotpassword" element={<ForgotPassword/>} />
-        <Route path="/user" element={<UserSidebar />} />
-        <Route path="" element={<PrivateRoutes />}>
-          <Route path="/admin" element={<AdminSidebar />}>
-            <Route index element={<Navigate to="dashboard" />} /> 
-            <Route path="dashboard" element={<AdminDashboard />} />
-            <Route path="usermanagement" element={<AdminUserManagement />} />
-            <Route path="modulemanagement" element={<AdminModuleManagement />} />      
-            <Route path="projectmanagement" element={<AdminProjectManagement />} />      
-            <Route path="taskmanagement" element={<AdminTaskManagement />} />      
-            <Route path="timelogmanagement" element={<AdminTimeLogManagement />} />      
-            <Route path="billingmanagement" element={<AdminBilling />} />      
-            <Route path="reportmanagement" element={<AdminReport />} />                     
-          </Route>
-          <Route path="/developer" element={<DeveloperSidebar />}>
-            <Route index element={<Navigate to="developertasklist" />} />
-            <Route path="developertasklist" element={<DeveloperTaskList />} />
-            <Route path="developertasklist/:taskId" element={<DeveloperTaskDetails />} /> 
-            <Route path="timelogger" element={<DeveloperTimeLogger />} />
-            <Route path="timeloglist" element={<DeveloperTimeLogList />} />
-          </Route>
-          <Route path="/manager" element={<ManagerSidebar />}>
-            <Route index element={<Navigate to="projectlist" />} />
-            <Route path="addprojectform" element={<AddProjectForm />} />
-            <Route path="projectlist" element={<ProjectList />} />
-            <Route path="projectlist/:id" element={<ProjectDetails />} />
-            <Route path="modulelist" element={<ModulesList />} />
-            <Route path="projectlist/:projectId/modules" element={<ModulesList />} />
-            <Route path="projectlist/:projectId/add-module" element={<AddModuleForm />} />
-            <Route path="projectlist/modules/:moduleId/tasks" element={<TaskList />} />
-            <Route path="projectlist/modules/:moduleId/add-task" element={<AddTaskForm />} />
-            <Route path="taskassignment" element={<TaskAssignment />} />
-          </Route>
-        </Route>
-      </Routes>
-    </div>
-  );
+    return (
+        <div className={location.pathname === "/login" || location.pathname === "/signup" ? "" : "app-wrapper"}>
+            <Routes>
+                <Route path="/" element={<Navigate to="/landingpage" replace />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
+                <Route path="/register" element={<RegisterSelection />} />
+                <Route path="/landingpage" element={<LandingPage />} />
+                <Route path="/resetpassword" element={<ResetPassword />} />
+                <Route path="/resetpassword/:token" element={<ResetPassword />} />
+                <Route path="/forgotpassword" element={<ForgotPassword />} />
+                <Route path="/user" element={<UserSidebar />} />
+                <Route path="" element={<PrivateRoutes />}>
+                    <Route path="/admin" element={<AdminSidebar />}>
+                        <Route index element={<Navigate to="dashboard" />} /> 
+                        <Route path="dashboard" element={<AdminDashboard />} />
+                        <Route path="usermanagement" element={<AdminUserManagement />} />
+                        <Route path="modulemanagement" element={<AdminModuleManagement />} />      
+                        <Route path="projectmanagement" element={<AdminProjectManagement />} />      
+                        <Route path="taskmanagement" element={<AdminTaskManagement />} />      
+                        <Route path="timelogmanagement" element={<AdminTimeLogManagement />} />      
+                        <Route path="billingmanagement" element={<AdminBilling />} />      
+                        <Route path="reportmanagement" element={<AdminReport />} />                     
+                    </Route>
+                    <Route path="/developer" element={<DeveloperSidebar />}>
+                        <Route index element={<Navigate to="developertasklist" />} />
+                        <Route path="developertasklist" element={<DeveloperTaskList />} />
+                        <Route path="developertasklist/:taskId" element={<DeveloperTaskDetails />} /> 
+                        <Route path="timelogger" element={<DeveloperTimeLogger />} />
+                        <Route path="timeloglist" element={<DeveloperTimeLogList />} />
+                    </Route>
+                    <Route path="/manager" element={<ManagerSidebar />}>
+                        <Route index element={<Navigate to="projectlist" />} />
+                        <Route path="addprojectform" element={<AddProjectForm />} />
+                        <Route path="projectlist" element={<ProjectList />} />
+                        <Route path="projectlist/:id" element={<ProjectDetails />} />
+                        <Route path="modulelist" element={<ModulesList />} />
+                        <Route path="projectlist/:projectId/modules" element={<ModulesList />} />
+                        <Route path="projectlist/:projectId/add-module" element={<AddModuleForm />} />
+                        <Route path="projectlist/modules/:moduleId/tasks" element={<TaskList />} />
+                        <Route path="projectlist/modules/:moduleId/add-task" element={<AddTaskForm />} />
+                        <Route path="taskassignment" element={<TaskAssignment />} />
+                    </Route>
+                </Route>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
